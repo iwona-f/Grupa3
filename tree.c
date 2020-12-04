cfa761519e1b00a2957608b9d2477a0196c2f827
Iwona <iwofig002@utp.edu.pl>
2020-12-04 15:45:30 +0100

    nie wiem o co chodzi

---
+++ tree.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+        int i,j,k,z,row;
+        printf("Podaj liczbe pieter\n");
+        scanf("%d",&row);
+        for (i = 0; i < row; i++) {
+            int space = row-1;
+            for(j=0;j<space;j++) {
+                printf(" ");
+            }
+            for(k=0;k<(i*2+1);k++){
+                printf("*");
+            }
+            printf(" \n");
+            }
+        for(z=0;z<(row-1);z++){
+            printf(" ");
+        }
+        printf("**");
+    return 0;
+}
